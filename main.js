2b8390ce22e4e963d8d91327178c2ce6b7d062b1
alexandr11147 <121605073+Alexandr11147@users.noreply.github.com>
2023-04-28 15:17:12 +0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const popup = document.querySelector('.popup_wrapper');
-const buttonElement = document.querySelector('.button')
-buttonElement.addEventListener('click', function (popupOn) {
-  popup.classList.toggle('visible');
-});
